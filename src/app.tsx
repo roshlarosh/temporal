b882466474ae6b8734d95ea9e7a5a14379a5de79
Roshan Thaabith <roshlarosh@gmail.com>
2019-12-10 22:58:24 +0000

    add input component

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import { TemporalIntro } from "./temporal-intro";
 import { InlineClickButton } from "./inline-button";
+import { InputTime } from "time-component";
 
 export class App extends React.Component {
   render() {
     return [
       <TemporalIntro stage={4}></TemporalIntro>,
-      <InlineClickButton name="START"></InlineClickButton>
+      <InlineClickButton name="START"></InlineClickButton>,
+      <InputTime value={this.state}></InputTime>
     ];
   }
 }
